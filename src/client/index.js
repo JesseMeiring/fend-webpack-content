a020b9531edb3a36be33f1145d4de54d90b33a40
Jesse Meiring <jessemeiring@gmail.com>
2020-10-25 22:28:54 -0400

    Update for 2.15

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,8 @@
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+//import html from './views/index.html'; //Importing an html page? That's obviously wrong... I guess...
+//import env from './file.json'
+
+console.log(checkForName);
+
 alert("I EXIST")
